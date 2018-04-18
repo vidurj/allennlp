95401516debf644ac11db4b2dc8ed8cc21901ae6
vidurj <vidur94@gmail.com>
2018-04-18 09:07:53 -0700

    inprogress

--- score.py
+++ score.py
@@ -5,8 +5,7 @@ import sys
 with open(sys.argv[1], 'r') as f:
     preds = f.read().splitlines()
 
-print(json.loads(preds[0])['predictions'])
-preds = ['num' + json.loads(x)['predictions'][0] for x in preds]
+preds = ['num' + json.loads(x)['predicted_tokens'][0] for x in preds]
 
 
 with open(sys.argv[2], 'r') as f:
