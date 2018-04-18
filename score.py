4c5bc868d62b1f7d13db50bec6332a2dab5586c6
vidurj <vidur94@gmail.com>
2018-04-18 09:05:42 -0700

    inprogress

---
+++ score.py
@@ -0,0 +1,19 @@
+import json
+import sys
+
+
+with open(sys.argv[1], 'r') as f:
+    preds = f.read().splitlines()
+
+preds = ['num' + json.loads(x)['preds'][0] for x in preds]
+
+
+with open(sys.argv[2], 'r') as f:
+    gold = f.read().split()
+
+assert len(gold) == len(preds), (len(gold), len(preds))
+
+print(preds[:20])
+print(gold[:20])
+
+print(sum([x == y for x, y in zip(preds, gold)]) / len(gold))
