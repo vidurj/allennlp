fe5e66e6a9e6707583beb49f794a95adc0badc54
vidurj <vidur94@gmail.com>
2018-04-20 13:22:28 -0700

    inprogress

--- allennlp/prepare_seq2seq_data.py
+++ allennlp/prepare_seq2seq_data.py
@@ -377,15 +377,15 @@ def create_sentence_aligned_data(alignments):
 
 
 if __name__ == '__main__':
-    prepare_synthetic_data()
-    # with open('/Users/vidurj/euclid/data/private/third_party/alg514/alg514_alignments.json',
-    #           'r') as f:
-    #     data = json.load(f)
-    #
-    # with open('allennlp/additional_annotations.json', 'r') as f:
-    #     additional_data = json.load(f)
-    #
-    # all_train_subsets = create_sentence_aligned_data(data[:-100])
-    # write_data(all_train_subsets + data[:-100], 'train.txt', randomize=True, num_iters=3)
-    # write_data(data[-100:], 'dev.txt', randomize=False, num_iters=1)
-    # write_data(data[-100:], 'test.txt', randomize=True, num_iters=1)
+    # prepare_synthetic_data()
+    with open('/Users/vidurj/euclid/data/private/third_party/alg514/alg514_alignments.json',
+              'r') as f:
+        data = json.load(f)
+
+    with open('allennlp/additional_annotations.json', 'r') as f:
+        additional_data = json.load(f)
+
+    all_train_subsets = create_sentence_aligned_data(data[:-100])
+    write_data(all_train_subsets + data[:-100], 'train.txt', randomize=True, num_iters=3)
+    write_data(data[-100:], 'dev.txt', randomize=False, num_iters=1)
+    write_data(data[-100:], 'test.txt', randomize=True, num_iters=1)
