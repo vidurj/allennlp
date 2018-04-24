1bf6a6a7274db0aa7e6601f54e1b0127275d0b1e
vidurj <vidur94@gmail.com>
2018-04-24 16:32:54 -0700

    temp

--- allennlp/models/encoder_decoders/Model.py
+++ allennlp/models/encoder_decoders/Model.py
@@ -261,7 +261,7 @@ class SimpleCopy(Model):
         complete_models = [model for model in models if model['action_list'][-1] == END_SYMBOL]
         complete_models.sort(key=lambda x: - x['cur_log_probability'])
         # print('total models', len(models), 'len complete models', len(complete_models))
-        output = '\n'.join([' '.join(model['action_list'][1:-1]) for model in complete_models]) + \
+        output = '\n'.join([str(model['cur_log_probabilty']) + ' ' + ' '.join(model['action_list'][1:-1]) for model in complete_models]) + \
                  '\n***\n'
         # print(' '.join(complete_models[0]['action_list'][1:-1]))
         return output
