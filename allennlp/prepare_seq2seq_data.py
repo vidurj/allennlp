5765ce6472e3f97c52ab59d30b076d913a9488d4
vidurj <vidur94@gmail.com>
2018-04-24 21:08:49 -0700

    temp

--- allennlp/prepare_seq2seq_data.py
+++ allennlp/prepare_seq2seq_data.py
@@ -361,9 +361,9 @@ if __name__ == '__main__':
         additional_data = json.load(f)
 
     # all_train_subsets = create_sentence_aligned_data(data[:-100])
-    # write_data(all_train_subsets + data[:-100], 'train.txt', randomize=True, num_iters=5)
-    # write_data(data[-100:], 'dev.txt', randomize=False, num_iters=1, silent=True)
-    # write_data(data[-100:], 'test.txt', randomize=True, num_iters=1)
+    write_data(data[:-100], 'train.txt', randomize=True, num_iters=50)
+    write_data(data[-100:], 'dev.txt', randomize=False, num_iters=1, silent=True)
+    write_data(data[-100:], 'test.txt', randomize=True, num_iters=1)
 
-    write_data(data[:3], 'train.txt', randomize=True, num_iters=500)
-    write_data(data[:3], 'dev.txt', randomize=True, num_iters=5)
+    # write_data(data[:3], 'train.txt', randomize=True, num_iters=500)
+    # write_data(data[:3], 'dev.txt', randomize=True, num_iters=5)
