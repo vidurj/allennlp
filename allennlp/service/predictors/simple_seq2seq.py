e0c82e9f86e5cf62c6debd743872b510edc59af3
vidurj <vidur94@gmail.com>
2018-04-25 16:59:32 -0700

    inprogress

--- allennlp/service/predictors/simple_seq2seq.py
+++ allennlp/service/predictors/simple_seq2seq.py
@@ -384,4 +384,5 @@ class SimpleSeq2SeqPredictor(Predictor):
         Expects JSON that looks like ``{"source": "..."}``.
         """
         source = json_dict["source"]
+        print(source)
         return self._dataset_reader.text_to_instance(source), {}
