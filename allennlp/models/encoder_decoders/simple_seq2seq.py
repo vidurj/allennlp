efdf968ddde7ca102d94d60ffd5dcb6054cad24a
vidurj <vidur94@gmail.com>
2018-04-30 20:50:08 -0700

    inprogress

--- allennlp/models/encoder_decoders/simple_seq2seq.py
+++ allennlp/models/encoder_decoders/simple_seq2seq.py
@@ -286,7 +286,10 @@ class SimpleSeq2Seq(Model):
         for sentence_number in range(len(sentence_number_to_text_field)):
             relevant_text_fields = sentence_number_to_text_field[sentence_number]
             source_tokens = relevant_text_fields['source_tokens']
-            print(' '.join([self.vocab.get_token_from_index(index, 'tokens') for index in source_tokens['tokens'].data.cpu().numpy()]))
+            temp = source_tokens['tokens'].data.cpu().numpy()
+            print('a' * 10, len(temp))
+
+            print(' '.join([self.vocab.get_token_from_index(index, 'tokens') for index in temp[0]]))
             source_mask = get_text_field_mask(source_tokens)
             embedded_input = self._source_embedder(source_tokens)
             batch_size, _, _ = embedded_input.size()
