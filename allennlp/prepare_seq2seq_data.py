b6357b813d2bcb87369d358bfc6438f146fa0cf5
vidurj <vidur94@gmail.com>
2018-05-02 18:02:26 -0700

    inprogress

--- allennlp/prepare_seq2seq_data.py
+++ allennlp/prepare_seq2seq_data.py
@@ -406,9 +406,9 @@ def synthetic_multisentence_data(num_samples, file_name):
     data_points = []
     start_to_token = {'a': '(Equals var1 num1)',
                       'the': '(Equals var1 (Plus var1 num1))',
-                      'cat': '(Equals var1 (MINUS var1 num1))',
-                      'crow': '(Equals var1 (TIMES var1 num1))',
-                      'hat': '(Equals var1 (DIV var1 num1))'}
+                      'cat': '(Equals var1 (Minus var1 num1))',
+                      'crow': '(Equals var1 (Times var1 num1))',
+                      'hat': '(Equals var1 (Div var1 num1))'}
     start_to_token = list(start_to_token.items())
     for _ in range(num_samples):
         (start, token) = random.choice(start_to_token)
