7d29ed465d09696e1b7ad26e881e559b16d9c848
vidurj <vidur94@gmail.com>
2018-05-14 14:16:46 -0700

    inprogress

--- allennlp/models/encoder_decoders/simple_seq2seq.py
+++ allennlp/models/encoder_decoders/simple_seq2seq.py
@@ -310,7 +310,7 @@ class SimpleSeq2Seq(Model):
             else:
                 if len(step_probabilities) > 0:
                     class_probabilities_np = step_predictions[-1].data.cpu().numpy()[0]
-                    print(class_probabilities_np.shape())
+                    print(class_probabilities_np.shape)
                     class_probabilities_np[corrupted_token_index] = 0
                     predicted_token_index = np.random.choice(range(len(class_probabilities_np)), p=class_probabilities_np)
                 else:
