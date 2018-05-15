09cabfe27e5b71a50cdf1fe0a9c58196ad452e8b
vidurj <vidur94@gmail.com>
2018-05-15 15:19:01 -0700

    inprogress

--- allennlp/models/encoder_decoders/simple_seq2seq.py
+++ allennlp/models/encoder_decoders/simple_seq2seq.py
@@ -296,7 +296,7 @@ class SimpleSeq2Seq(Model):
         step_probabilities = []
         step_predictions = []
         corrupted_token_index = self.vocab.get_token_index('<corrupted>', self._target_namespace)
-        if self.training:
+        if self.training and random.random() > 0.5:
             corrupted_index = random.randint(2, num_decoding_steps)
         else:
             corrupted_index = num_decoding_steps + 100000
